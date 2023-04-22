61db5d82eb9766167d17e3c5f8d2cb08b6e3f477
RodolfoD <rdescazeaux@gmail.com>
2023-04-21 21:23:38 -0400

    modificacion de clase

--- src/test/java/com/tempdevops/demo/MiClase.java
+++ src/test/java/com/tempdevops/demo/MiClase.java
@@ -1,6 +1,18 @@
 package com.tempdevops.demo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MiClase {
 
+  @Test
+  @DisplayName("Prueba para verificar la suma de dos números")
+  public void testSuma() {
+    int a = 5;
+    int b = 10;
+    int resultado = a + b;
+    Assertions.assertEquals(15, resultado);
+  }
   
 }
